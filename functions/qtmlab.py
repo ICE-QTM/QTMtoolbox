fcbec03fe1574178d3ca5b8cfb8c2eb0320ce6e4
Joris Voerman <jorisvoerman@gmail.com>
2019-03-13 09:57:22 +0000

    Added qtmlab.setsens command. Updated the filename checker to append an incremental number instead of _1_1_1_1 etc.

--- functions/qtmlab.py
+++ functions/qtmlab.py
@@ -7,7 +7,7 @@ Available functions:
     measure()
     sweep(device, variable, start, stop, rate, npoints, filename)
 
-Version 1.41 (2019-03-07)
+Version 1.51 (2019-03-12) -- Edited by Joris
 Daan Wielens - PhD at ICE/QTM
 University of Twente
 daan@daanwielens.com
@@ -142,11 +142,16 @@ def sweep(device, variable, start, stop, rate, npoints, filename, sweepdev=None,
     if md is None:
         md = meas_dict
 
-    # Initialise datafile and write header
+    # Initialise datafile
+    append_no = 0;
     while os.path.isfile(filename):
-        print('The file already exists. Appending "_1" to the filename.')
+        append_no += 1 #Count the number of times the file already existed
         filename = filename.split('.')
-        filename = filename[0] + '_1.' + filename[1]
+        if append_no == 1: #The first time the program finds the unedited filename. We save it as the base
+            filename_base = filename[0]
+        filename = filename_base + '_' + str(append_no) +'.' + filename[1] #add "_N" to the filename where N is the number of loop iterations
+        if os.path.isfile(filename) == False: #Only when the newly created filename doesn't exist: inform the user. The whileloop stops.
+            print('The file already exists. Filename changed to: ' + filename)
     # Get specified variable name, or use default
     if sweepdev is None:
         sweepdev = 'sweepdev'
@@ -204,7 +209,6 @@ def waitfor(device, variable, setpoint, threshold=0.05, tmin=60):
         if t_stable >= tmin:
             stable = True
 
-
 def record(dt, npoints, filename, md=None):
     """
     The record command records data with a time interval of <dt> seconds. It
@@ -213,6 +217,17 @@ def record(dt, npoints, filename, md=None):
     # Trick to make sure that dictionary loading is handled properly at startup
     if md is None:
         md = meas_dict
+        
+    # Initialise datafile
+    append_no = 0;
+    while os.path.isfile(filename):
+        append_no += 1 #Count the number of times the file already existed
+        filename = filename.split('.')
+        if append_no == 1: #The first time the program finds the unedited filename. We save it as the base
+            filename_base = filename[0]
+        filename = filename_base + '_' + str(append_no) +'.' + filename[1] #add "_N" to the filename where N is the number of loop iterations
+        if os.path.isfile(filename) == False: #Only when the newly created filename doesn't exist: inform the user. The whileloop stops.
+            print('The file already exists. Filename changed to: ' + filename)
 
     # Build header
     header = 'time'
@@ -229,3 +244,38 @@ def record(dt, npoints, filename, md=None):
         with open(filename, 'a') as file:
             file.write(datastr + '\n')
         time.sleep(dt)
+
+def setsens(device, vrange_in_mV):
+    """
+    The write command sets a new lock-in sensitivity.
+    It translates voltage ranges to lock-in sensitivity numbers.
+    It achieves this by sorting the user value into the array of numbers. The lock-in
+    should be send the index number of this array!
+    """
+    sens = np.array([2e-9, 5e-9, 1e-8, 2e-8, 5e-8, 1e-7, 2e-7, 5e-7, 1e-6, 2e-6, 5e-6, 1e-5, 2e-5, 5e-5, 1e-4, 2e-4, 5e-4, 1e-3, 2e-3, 5e-3, 1e-2, 2e-2, 5e-2, 1e-1, 2e-1, 5e-1, 1])
+    vrange = vrange_in_mV/1000  #User input is given in mV
+    check = 19 #19 is 5 mV, which is a good starting point to start the sorting.
+    Sorted = False
+    while Sorted == False:
+        #This statement checks if we have found the correct place to put our value
+        if vrange <= sens[check] and vrange >= sens[check-1]:
+            Sorted = True
+            if vrange == sens[check-1]:
+                write_command = getattr(device, 'write_sens')
+                write_command(check-1)
+            else:
+                write_command = getattr(device, 'write_sens')
+                write_command(check)
+            return
+        #If the value was too small we move to lower indices
+        elif vrange < sens[check]:
+            check = check-1
+        #If the value was too high we move to higher indices
+        else:
+            check = check+1
+        #If the algorithm goes off the rails: Catch it and provide feedback to the user
+        if check == 0 or check == 28:
+            Sorted = True
+            print('Unable to determine the sensitivity. Correct syntax: qtmlab.setsens(device,range)')
+            write_command = getattr(device, 'write_sens')
+            write_command(check)
