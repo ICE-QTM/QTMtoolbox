9b8eb337633215415e4a408312eb7cd537af096b
Daan Wielens <github@daanwielens.com>
2022-04-05 17:19:37 +0200

    Waveform capture possible from instrument driver itself
    The waveform capture has two options:
    - 500 points (1 bit per data points = 256 bins) --> 8 bit resolution
    - 10000 points (2 bits per data point = 65536 bins) --> "full" 9 bit resolution

--- instruments/TekTDS3012C.py
+++ instruments/TekTDS3012C.py
@@ -3,18 +3,21 @@
 Module to interact with a Tektronix AFG1022.
 Uses pyVISA to communicate with the USB/GPIB device.
 
-Version 1.0 (2018-10-09)
-Daan Wielens - PhD at ICE/QTM
+Version 2.0 (2022-04-05)
+Daan Wielens - Researcher at ICE/QTM
 University of Twente
 daan@daanwielens.com
 """
 
 import pyvisa as visa
+import numpy as np
+import ast
+from struct import unpack
 
 class WrongInstrErr(Exception):
     """
     A connection was established to the instrument, but the instrument
-    is not a Tektronix TDS 3012C. Please retry with the correct
+    is not a Tektronix TDS 3012 series. Please retry with the correct
     GPIB address. Make sure that each device has an unique address.
     """
     pass
@@ -28,8 +31,8 @@ class TekTDS3012C:
         # Check if device is really a Tektronix AFG1022
         resp = self.visa.query('*IDN?')
         model = resp.split(',')[1]
-        if model != 'TDS 3012C':
-            raise WrongInstrErr('Expected Tektronix TDS 3012C, got {}'.format(resp))
+        if 'TDS 3012' not in model:
+            raise WrongInstrErr('Expected Tektronix TDS 3012 series, got {}'.format(resp))
 
     def get_iden(self):
         resp = str(self.visa.query('*IDN?'))
@@ -37,35 +40,145 @@ class TekTDS3012C:
 
     def close(self):
         self.visa.close()
+        
+    def query(self, val):
+        return self.visa.query(val)
+    
+    def write(self, val):
+        self.visa.write(val)
 
+    # Read measurements
     def read_meas1(self):
-        resp = self.visa.query('MEASU:MEAS1:VAL?')
+        resp = self.query('MEASU:MEAS1:VAL?')
         resp = float(resp.split(' ')[1])
         return resp
 
     def read_meas2(self):
-        resp = self.visa.query('MEASU:MEAS2:VAL?')
+        resp = self.query('MEASU:MEAS2:VAL?')
         resp = float(resp.split(' ')[1])
         return resp
 
     def read_meas3(self):
-        resp = self.visa.query('MEASU:MEAS3:VAL?')
+        resp = self.query('MEASU:MEAS3:VAL?')
         resp = float(resp.split(' ')[1])
         return resp
 
     def read_meas4(self):
-        resp = self.visa.query('MEASU:MEAS4:VAL?')
+        resp = self.query('MEASU:MEAS4:VAL?')
         resp = float(resp.split(' ')[1])
         return resp
 
+    # Divider settings
     def write_horzdiv(self, val):
         val = float(val)
-        self.visa.write('HOR:MAI:SCA ' + str(val) + '\n')
+        self.write('HOR:MAI:SCA ' + str(val) + '\n')
 
     def write_vertdiv1(self, val):
         val = float(val)
-        self.visa.write('CH1:SCA ' + str(val) + '\n')
+        self.write('CH1:SCA ' + str(val) + '\n')
 
     def write_vertdiv2(self, val):
         val = float(val)
-        self.visa.write('CH2:SCA ' + str(val) + '\n')
+        self.write('CH2:SCA ' + str(val) + '\n')
+
+    def read_horzdiv(self):
+        return float(self.query('HOR:MAI:SCA?').strip('\n'))
+    
+    def read_vertdiv1(self):
+        return float(self.query('CH1:SCA?').strip('\n'))
+    
+    def read_vertdiv2(self):
+        return float(self.query('CH2:SCA?').strip('\n'))
+    
+    # Preamble acquisition
+    def get_pre1(self):
+        self.write('DATA:SOU CH1')
+        data = self.query('WFMP?').split(';')
+        ymult = float(data[12])
+        yzero = float(data[13])
+        yoff = float(data[14])
+        xinc = float(data[8])
+        return ymult, yzero, yoff, xinc
+        
+    def get_pre2(self):
+        self.write('DATA:SOU CH2')
+        data = self.query('WFMP?').split(';')
+        ymult = float(data[12])
+        yzero = float(data[13])
+        yoff = float(data[14])
+        xinc = float(data[8])
+        return ymult, yzero, yoff, xinc   
+    
+    # Get number of points (horizontal resolution)
+    def read_npts1(self):
+        self.write('DATA:SOU CH1')
+        return int(self.query('WFMP:NR_Pt?'))
+        
+    def read_npts2(self):
+        self.write('DATA:SOU CH1')
+        return int(self.query('WFMP:NR_Pt?'))
+    
+    def write_npts(self, val):
+        if val in [500, 10000]:
+            self.write('HOR:RECO ' + str(val))
+        else:
+            raise ValueError('The number of points can either be 500 or 10000.')
+            
+    # Get waveform
+    def get_wav1(self):
+        self.write('DATA:SOU CH1')
+        # If 500 data points are chosen, the scope does 'fast trigger' / 1 bit data (256 bins)
+        if self.read_npts1() == 500:
+            self.write('DATA:WIDTH 1')
+            self.write('DATA:ENC RPB') # Requires %sB for unpack
+            pre = self.get_pre1()
+            self.write('CURVE?')
+            data = self.visa.read_raw()
+             
+            headerlen = 5 # We expect the header to be '#3500' (#, 3 bytes required to print the number 500, 500 itself; 1 bits per data point)
+            ADCwave = data[headerlen:-1]
+            ADCwave = np.array(unpack('%sB' % len(ADCwave), ADCwave))    
+        # If 10000 points are chosen ('normal'), we get 2 bit data (65536 bins)
+        else:
+            self.write('DATA:WIDTH 2')
+            self.write('DATA:ENC SRP') # Requires %sH for unpack
+            pre = self.get_pre1()
+            self.write('CURVE?')
+            data = self.visa.read_raw()
+
+            headerlen = 7 # '#520000' (#, 4 bytes for 10000*2, 20000 itself; 2 bits per data point)
+            ADCwave = data[headerlen:-1]
+            ADCwave = np.array(unpack('%sH' % int(len(ADCwave)/2), ADCwave))
+         
+        V = (ADCwave - pre[2]) * pre[0] + pre[1]
+        t = np.arange(0, pre[3] * len(V), pre[3])
+        return t, V       
+     
+    def get_wav2(self):
+        self.write('DATA:SOU CH2')
+        # If 500 data points are chosen, the scope does 'fast trigger' / 1 bit data (256 bins)
+        if self.read_npts1() == 500:
+            self.write('DATA:WIDTH 1')
+            self.write('DATA:ENC RPB') # Requires %sB for unpack
+            pre = self.get_pre2()
+            self.write('CURVE?')
+            data = self.visa.read_raw()
+             
+            headerlen = 5 # We expect the header to be '#3500' (#, 3 bytes required to print the number 500, 500 itself; 1 bits per data point)
+            ADCwave = data[headerlen:-1]
+            ADCwave = np.array(unpack('%sB' % len(ADCwave), ADCwave))    
+        # If 10000 points are chosen ('normal'), we get 2 bit data (65536 bins)
+        else:
+            self.write('DATA:WIDTH 2')
+            self.write('DATA:ENC SRP') # Requires %sH for unpack
+            pre = self.get_pre2()
+            self.write('CURVE?')
+            data = self.visa.read_raw()
+
+            headerlen = 7 # '#520000' (#, 4 bytes for 10000*2, 20000 itself; 2 bits per data point)
+            ADCwave = data[headerlen:-1]
+            ADCwave = np.array(unpack('%sH' % int(len(ADCwave)/2), ADCwave))
+         
+        V = (ADCwave - pre[2]) * pre[0] + pre[1]
+        t = np.arange(0, pre[3] * len(V), pre[3])
+        return t, V 
