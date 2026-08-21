8601e0ef957ae4c2ff95035805b099a759e3566f
Daan Wielens <github@daanwielens.com>
2026-08-21 15:49:12 +0200

    Bugfixes and implementation of first basic commands

--- instruments/ICEoxfordNexus.py
+++ instruments/ICEoxfordNexus.py
@@ -5,9 +5,35 @@ Uses TCP/IP sockets to communicate with the device.
 
 Based on example code provided by ICEoxford.
 
-Version 0.1 (2026-08-21)
+------------------------------------------------------------------
+Note: this driver is tailored to the NE/ICE DryICE system.
+Please change channel names etc. if you use it for a 
+different lab/system.
+------------------------------------------------------------------
+
+Version 0.2 (2026-08-21)
 Daan Wielens - Researcher at ICE/QTM
 University of Twente
+
+------------------------------------------------------------------
+Note: the workflow for this instrument is different than for 
+most GPIB instruments. The streaming service needs to be started
+before one can get data from the machine.
+
+The following code will set up the device and give results:
+    
+    from instruments.ICEoxfordNexus import *
+    ice = ICEoxfordNexus('123.456.78.90')
+    ice.start_streaming()
+    ice.read_tempA()
+    
+Upon finishing an experiment, a clean exit is performed by
+executing the following code:
+    
+    ice.stop_streaming()
+    ice.close()
+
+------------------------------------------------------------------
 """
 
 import socket
@@ -25,23 +51,28 @@ class ICEoxfordNexus:
         self.s = socket.socket()
         self.s.connect((IPaddress, port))
         
+        self.channel_names = None
+        
     def query(self, val):
         self.s.sendall((str(val) + '\r\n').encode('ascii'))
         resp = self.s.recv(262144).decode('ascii', errors='replace')
         return resp.strip()
     
     def start_streaming(self):
+        print('<!> Nexus software: start streaming...')
         print(self.query('Set_Remote'))
         print(self.query('Start_Streaming(Interval=1000,Port=6341,Channels=All,Status=All)'))
-        
+        self.channel_names = self.get_channel_names()
+           
     def stop_streaming(self):
+        print('<!> Nexus software: stop streaming...')
         print(self.query('Stop_Streaming'))
     
     def close(self):
         print(self.query('Set_Local'))
         self.s.close()
     
-    def get_text_inside_brackets(text):
+    def get_text_inside_brackets(self, text):
         """
         Return the text inside brackets, e.g. Name(A,B,C) -> A,B,C.
         Function by ICEoxford.
@@ -76,9 +107,9 @@ class ICEoxfordNexus:
      
         return f"{value:.6e}"
         
-        def get_channel_names(self):
-            resp = self.get_text_inside_brackets(self.query('Get_Stream_Names'))
-            return [name.strip() for name in resp.split(",") if name.strip()]
+    def get_channel_names(self):
+        resp = self.get_text_inside_brackets(self.query('Get_Stream_Names'))
+        return [name.strip() for name in resp.split(",") if name.strip()]
         
     def get_values(self):
         """
@@ -105,6 +136,34 @@ class ICEoxfordNexus:
                 values.append(None)
      
         return values
+
+    # Temperature readout -----------------------------------------------------   
+    def read_tempA(self):
+        return float(self.get_values()[self.channel_names.index('A')])
+    
+    def read_tempB(self):
+        return float(self.get_values()[self.channel_names.index('B')])
+    
+    def read_tempC(self):
+        return float(self.get_values()[self.channel_names.index('C')])
+    
+    def read_tempD(self):
+        return float(self.get_values()[self.channel_names.index('D')])
         
+    def read_tempD2(self):
+        return float(self.get_values()[self.channel_names.index('D2')])
+
+    def read_tempD3(self):
+        return float(self.get_values()[self.channel_names.index('D3')])
+
+    def read_tempD4(self):
+        return float(self.get_values()[self.channel_names.index('D4')])
+
+    def read_tempD5(self):
+        return float(self.get_values()[self.channel_names.index('D5')])
+    
+    # Pressure readout --------------------------------------------------------
+    def read_circ(self):
+        return float(self.get_values()[self.channel_names.index('Circulation_Pressure')])
         
         
